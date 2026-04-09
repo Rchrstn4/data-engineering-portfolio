c0ac28c4ae1b1030b21ee8304e98f43fac8178dd
Rchrstn4 <45026692+Rchrstn4@users.noreply.github.com>
2026-04-09 08:54:32 -0500

    Update transform_customers.py

--- project1_aws_etl_pipeline/glue_jobs/transform_customers.py
+++ project1_aws_etl_pipeline/glue_jobs/transform_customers.py
@@ -2,19 +2,23 @@ import sys
 from awsglue.context import GlueContext
 from pyspark.context import SparkContext
 from awsglue.utils import getResolvedOptions
+from pyspark.sql.functions import col, substring
 
-args = getResolvedOptions(sys.argv, ['S3_INPUT', 'S3_OUTPUT'])
-input_path = args['S3_INPUT']
-output_path = args['S3_OUTPUT']
+args = getResolvedOptions(sys.argv, ["S3_INPUT", "S3_OUTPUT"])
 
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+input_path = args["S3_INPUT"]
+output_path = args["S3_OUTPUT"]
+
+sc = SparkContext.getOrCreate()
+glue_context = GlueContext(sc)
+spark = glue_context.spark_session
 
 df = spark.read.csv(input_path, header=True, inferSchema=True)
 
-df_clean = df.dropDuplicates() \
-             .withColumnRenamed("customer_id", "id") \
-             .withColumn("signup_year", df.signup_date.substr(1,4))
+df_clean = (
+    df.dropDuplicates()
+      .withColumnRenamed("customer_id", "id")
+      .withColumn("signup_year", substring(col("signup_date"), 1, 4))
+)
 
 df_clean.write.mode("overwrite").parquet(output_path)
