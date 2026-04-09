d53d1cf0dd45cb2d0e39f012b064a01f3d3b4cb4
Rchrstn4 <45026692+Rchrstn4@users.noreply.github.com>
2026-04-09 08:50:43 -0500

    Update etl_pipeline_dag.py

--- project1_aws_etl_pipeline/dags/etl_pipeline_dag.py
+++ project1_aws_etl_pipeline/dags/etl_pipeline_dag.py
@@ -4,27 +4,35 @@ from airflow.providers.amazon.aws.operators.redshift import RedshiftSQLOperator
 from datetime import datetime
 
 with DAG(
-    "aws_etl_pipeline",
+    dag_id="aws_etl_pipeline",
     start_date=datetime(2024, 1, 1),
     schedule_interval="@daily",
-    catchup=False
+    catchup=False,
+    tags=["etl", "aws", "portfolio"]
 ):
 
     run_glue = GlueJobOperator(
         task_id="transform_customers",
-        job_name="glue-transform-customers",
-        script_location="s3://my-etl-bucket/scripts/transform_customers.py",
-        region_name="us-east-1"
+        job_name="demo-glue-transform-customers",
+        script_location="s3://demo-etl-pipeline-bucket/scripts/transform_customers.py",
+        region_name="us-east-1",
+        iam_role_name="AWSGlueServiceRole-Demo",
+        create_job_kwargs={
+            "GlueVersion": "4.0",
+            "NumberOfWorkers": 2,
+            "WorkerType": "G.1X"
+        }
     )
 
     load_to_redshift = RedshiftSQLOperator(
         task_id="load_to_redshift",
         sql="""
-        COPY customers
-        FROM 's3://my-etl-bucket/clean/customers/'
-        IAM_ROLE 'arn:aws:iam::YOUR_NUMBER:role/MyRedshiftRole'
+        COPY public.customers
+        FROM 's3://demo-etl-pipeline-bucket/clean/customers/'
+        IAM_ROLE 'arn:aws:iam::111122223333:role/RedshiftS3AccessRoleDemo'
         FORMAT AS PARQUET;
-        """
+        """,
+        redshift_conn_id="redshift_default"
     )
 
     run_glue >> load_to_redshift
